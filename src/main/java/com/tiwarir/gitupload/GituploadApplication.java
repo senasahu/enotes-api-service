eded124306ba16e78e8ddbabd74c9a069fbb2f67
senasahu <senasahu99@gmail.com>
2025-06-08 21:09:56 +0530

    inital commit

---
+++ src/main/java/com/tiwarir/gitupload/GituploadApplication.java
@@ -0,0 +1,13 @@
+package com.tiwarir.gitupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GituploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GituploadApplication.class, args);
+	}
+
+}
